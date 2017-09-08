77b5597f3b4d479769bde8338ee9777bc324df71
= <=>
2017-09-08 19:31:18 -0400

    started working on project 1

--- list.c
+++ list.c
@@ -1,1 +1,12 @@
 /*function bodies (implementations) in a source file “list.c”.*/
+#if 0
+	cool way to comment
+#endif
+
+#include <stdlib.h>
+#include "list.h"
+int main(){
+
+	return 0;
+
+}
--- list.h
+++ list.h
@@ -1,2 +1,8 @@
 /*Put any struct definitions, typedefs and the above function prototypes in a header file
 “list.h”*/
+
+struct myList
+{
+	int data;
+	struct myList* next;	
+};
